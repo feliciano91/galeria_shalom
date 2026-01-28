dc2df494f5d84a074de38ff56b7143373cd1299a
Feliciano Afonso Sa <sijulcaya@gmail.com>
2026-01-27 22:20:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,16 +25,14 @@ CORS(app, resources={
 # ----------------------
 # CONFIG BANCO (Render)
 # ----------------------
-DATABASE_URL = (
-    "postgresql://galeria_shalom_db_user:"
-    "A9vUujpt3sM1D01UNz3x4fJi8QWnejTo@"
-    "dpg-d55iorumcj7s73fcj4dg-a.oregon-postgres.render.com:5432/"
-    "galeria_shalom_db"
-    "?sslmode=require"
-)
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db_connection():
-    return psycopg2.connect(DATABASE_URL)
+    return psycopg2.connect(
+        DATABASE_URL,
+        sslmode="require",
+        connect_timeout=10
+    )
 
 # =======================
 # MANICURE
